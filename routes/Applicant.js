77aa1638e63d4dad99010ed463edf947c353d5d5
nsarafa <nicksarafa@gmail.com>
2017-03-26 13:18:39 -0400

    Education route init

--- routes/Applicant.js
+++ routes/Applicant.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose')
 let Applicant = require('../models/Applicant')
 
-// GET /Applicant route to retrieve all the applicants.
+// GET /Applicant route to retrieve all the applicants
 function getApplicants(req, res) {
     // Query the DB and if no errors, send all the Applicants
     let query = Applicant.find({})
@@ -11,18 +11,18 @@ function getApplicants(req, res) {
     })
 }
 
-// POST /Applicant to save a new applicant.
+// POST /Applicant to save a new applicant
 function postApplicant(req, res) {
     // Creates a new Applicant
     var newApplicant = new Applicant(req.body)
     // Save new applicant to the DB
     newApplicant.save((err,Applicant) => {
         if (err) res.send(err)
-        else res.json({message: "Applicant successfully added!", Applicant })
+        else res.json({ message: 'Applicant successfully added!', Applicant })
     })
 }
 
-// GET /Applicant/:id route to retrieve a applicant given its id.
+// GET /Applicant/:id route to retrieve an applicant given its id
 function getApplicant(req, res) {
     Applicant.findById(req.params.id, (err, Applicant) => {
         if(err) res.send(err)
@@ -30,14 +30,14 @@ function getApplicant(req, res) {
     })     
 }
 
-// DELETE /Applicant/:id to delete a applicant given its id.
+// DELETE /Applicant/:id to delete an applicant given its id
 function deleteApplicant(req, res) {
     Applicant.remove({_id : req.params.id}, (err, result) => {
-        res.json({ message: "Applicant successfully deleted!", result })
+        res.json({ message: 'Applicant successfully deleted!', result })
     })
 }
 
-// PUT /Applicant/:id to updatea a applicant given its id
+// PUT /Applicant/:id to update an applicant given its id
 function updateApplicant(req, res) {
     Applicant.findById({_id: req.params.id}, (err, Applicant) => {
         if(err) res.send(err)
---
+++ routes/Education.js
@@ -0,0 +1,52 @@
+let mongoose = require('mongoose')
+let Education = require('../models/Education')
+
+/**
+ * @todo nest Education within Applicant. I don't think it needs its own route
+ */
+
+// GET /Education route to retrieve all educations
+function getEducations(req, res) {
+    // query the DB and if no errors, send all the Applicants
+    let query = Education.find({})
+    query.exec((err, Educations) => {
+        if(err) res.send(err)
+        res.json(Educations)
+    })
+}
+
+// POST /Education to save new education
+function postEducation(req, res) {
+    // creates a new education
+    var newEducation = new Education(req.body)
+    // save new education to db
+    newEducation.save((err, Education) => {
+        if(err) res.send(err)
+        else res.json({ message: 'Education successfully added!', Education })
+    })
+}
+
+// GET /Education/:id to retrieve Education(s) given its id
+function getEducation(req, res) {
+    Education.findById(req.params.id, (err, Education) => {
+        if(err) res.send(err)
+        res.json(Education)
+    })
+}
+
+// DELETE /Education/:id to delete an education given its id
+function deleteEducation(req, res) {
+    Education.remove({_id: req.params.id}, (err, result) => {
+        res.json({ message: 'Education successfully deleted', result })
+    })
+}
+
+// UPDATE /Education/:id
+function updateEducation(req, res) {
+    Education.findById({_id: req.params.id}, (err, Education) => {
+        if(err) res.send(err)
+        res.json({ message: 'Education updated!' })
+    })
+}
+
+module.exports = { getEducations, postEducation, getEducation, deleteEducation, updateEducation }
