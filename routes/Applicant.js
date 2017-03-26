8177cfee14ea46319a1451cd09d049a134e10279
nsarafa <nicksarafa@gmail.com>
2017-03-26 00:46:59 -0400

    update Applicant routes annotations

--- routes/Applicant.js
+++ routes/Applicant.js
@@ -1,59 +1,43 @@
 let mongoose = require('mongoose')
 let Applicant = require('../models/Applicant')
 
-/*
- * GET /Applicant route to retrieve all the applicants.
- */
+// GET /Applicant route to retrieve all the applicants.
 function getApplicants(req, res) {
-    //Query the DB and if no errors, send all the Applicants
+    // Query the DB and if no errors, send all the Applicants
     let query = Applicant.find({})
     query.exec((err, Applicants) => {
         if(err) res.send(err)
-        //If no errors, send them back to the client
         res.json(Applicants)
     })
 }
 
-/*
- * POST /Applicant to save a new applicant.
- */
+// POST /Applicant to save a new applicant.
 function postApplicant(req, res) {
-    //Creates a new Applicant
+    // Creates a new Applicant
     var newApplicant = new Applicant(req.body)
-    //Save it into the DB.
+    // Save new applicant to the DB
     newApplicant.save((err,Applicant) => {
-        if(err) {
-            res.send(err)
-        }
-        else { //If no errors, send it back to the client
-            res.json({message: "Applicant successfully added!", Applicant })
-        }
+        if (err) res.send(err)
+        else res.json({message: "Applicant successfully added!", Applicant })
     })
 }
 
-/*
- * GET /Applicant/:id route to retrieve a applicant given its id.
- */
+// GET /Applicant/:id route to retrieve a applicant given its id.
 function getApplicant(req, res) {
     Applicant.findById(req.params.id, (err, Applicant) => {
         if(err) res.send(err)
-        //If no errors, send it back to the client
         res.json(Applicant)
     })     
 }
 
-/*
- * DELETE /Applicant/:id to delete a applicant given its id.
- */
+// DELETE /Applicant/:id to delete a applicant given its id.
 function deleteApplicant(req, res) {
     Applicant.remove({_id : req.params.id}, (err, result) => {
         res.json({ message: "Applicant successfully deleted!", result })
     })
 }
 
-/*
- * PUT /Applicant/:id to updatea a applicant given its id
- */
+// PUT /Applicant/:id to updatea a applicant given its id
 function updateApplicant(req, res) {
     Applicant.findById({_id: req.params.id}, (err, Applicant) => {
         if(err) res.send(err)
@@ -64,5 +48,4 @@ function updateApplicant(req, res) {
     })
 }
 
-//export all the functions
 module.exports = { getApplicants, postApplicant, getApplicant, deleteApplicant, updateApplicant }
