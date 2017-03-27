38f7eaeb5f312bf1ab710c46840cc2c3355b878e
nsarafa <nicksarafa@gmail.com>
2017-03-27 13:53:04 -0400

    Experience Schema init

---
+++ models/Experience.js
@@ -0,0 +1,19 @@
+mongoose = require('mongoose')
+Schema = mongoose.Schema
+
+/**
+ * @todo introduce location to Schema
+ * @todo introduce countOfMonths to Schema
+ * @todo introduce isCurrentEmployer
+ */
+let ExperienceSchema = new Schema({
+    organizationName: String,
+    title: String,
+    startMonth: String,
+    startYear: String,
+    endMonth: String,
+    endYear: String,
+    description: String,
+})
+
+module.exports = mongoose.model('Experience', ExperienceSchema)
