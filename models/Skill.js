7324f53d44db80e5ef97ca2d52ab193b68b2b1a2
nsarafa <nicksarafa@gmail.com>
2017-03-27 18:46:09 -0400

    Skill route, test, server setup init

--- models/Skill.js
+++ models/Skill.js
@@ -1,11 +1,20 @@
 let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
-Skill = new Schema({
+let SkillSchema = new Schema({
     name: {
         type: String,
         required: true,
-    }
+    },
+})
+
+/**
+ * @todo double check to make sure this method of setting Date schema is sound
+ */
+SkillSchema.pre('save', next => {
+    now = new Date()
+    if(!this.createdAt) this.createdAt = now
+    next()
 })
 
 module.exports = mongoose.model('Skill', SkillSchema)
