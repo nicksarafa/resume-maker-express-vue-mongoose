22d5d75222e57fd8f6c196db7b45a85696382996
nsarafa <nicksarafa@gmail.com>
2017-03-27 15:31:11 -0400

    static array util init && listOfYear/listOfMonth static arrays init

--- models/Education.js
+++ models/Education.js
@@ -1,11 +1,9 @@
 let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
-/**
- * @todo remove currentYear nonsense
- * @todo figure out best way to handle setting years
- * `needs design`
- */
+// import years from static array util
+let listOfYears = require('../utils/static').listOfYears
+
 let EducationSchema = new Schema({
     schoolName: {
         type: String,
@@ -16,23 +14,13 @@ let EducationSchema = new Schema({
     extracurriculars: String,
     description: String,
     startYear: {
-        type: Date,
-        default: Date.getFullYear,
+        type: String,
+        enum: listOfYears,
     },
     endYear: {
-        type: Date,
-        default: Date.getFullYear,
+        type: String,
+        enum: listOfYears,
     },
-})
-
-/**
- * @todo double check to make sure this method of setting Date schema is sound
- */
-EducationSchema.pre('save', next => {
-    currentYear = new Date().getFullYear()
-    if(!this.startYear) this.startYear = currentYear
-    else if(!this.endYear) this.endYear = currentYear
-    next()
 })
 
 module.exports = mongoose.model('Education', EducationSchema)
