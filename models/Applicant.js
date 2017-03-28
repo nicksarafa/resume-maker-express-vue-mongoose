6e5b4e5c3e49618801a63b0a4d1d83688793e289
nsarafa <nicksarafa@gmail.com>
2017-03-28 18:34:20 -0400

    get Applicant up to speed with project structure

--- models/Applicant.js
+++ models/Applicant.js
@@ -2,8 +2,6 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
 /**
- * @todo remove createdAt
- * @todo remove university as its now in Education
  * @todo add optional user website url
  */
 let ApplicantSchema = new Schema({
@@ -13,20 +11,6 @@ let ApplicantSchema = new Schema({
     },
     email: String,
     phone: String,
-    university: String,
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-})
-
-/**
- * @todo double check to make sure this method of setting Date schema is sound
- */
-ApplicantSchema.pre('save', next => {
-    now = new Date()
-    if(!this.createdAt) this.createdAt = now
-    next()
 })
 
 module.exports = mongoose.model('Applicant', ApplicantSchema)
--- routes/Applicant.js
+++ routes/Applicant.js
@@ -1,9 +1,7 @@
 let mongoose = require('mongoose')
 let Applicant = require('../models/Applicant')
 
-// GET /Applicant route to retrieve all the applicants
 function getApplicants(req, res) {
-    // Query the DB and if no errors, send all the Applicants
     let query = Applicant.find({})
     query.exec((err, Applicants) => {
         if(err) res.send(err)
@@ -11,18 +9,14 @@ function getApplicants(req, res) {
     })
 }
 
-// POST /Applicant to save a new applicant
 function postApplicant(req, res) {
-    // Creates a new Applicant
     var newApplicant = new Applicant(req.body)
-    // Save new applicant to the DB
     newApplicant.save((err,Applicant) => {
         if (err) res.send(err)
         else res.json({ message: 'Applicant successfully added!', Applicant })
     })
 }
 
-// GET /Applicant/:id route to retrieve an applicant given its id
 function getApplicant(req, res) {
     Applicant.findById(req.params.id, (err, Applicant) => {
         if(err) res.send(err)
@@ -30,20 +24,18 @@ function getApplicant(req, res) {
     })     
 }
 
-// DELETE /Applicant/:id to delete an applicant given its id
 function deleteApplicant(req, res) {
-    Applicant.remove({_id : req.params.id}, (err, result) => {
+    Applicant.remove({ _id : req.params.id }, (err, result) => {
         res.json({ message: 'Applicant successfully deleted!', result })
     })
 }
 
-// PUT /Applicant/:id to update an applicant given its id
 function updateApplicant(req, res) {
-    Applicant.findById({_id: req.params.id}, (err, Applicant) => {
+    Applicant.findById({ _id: req.params.id }, (err, Applicant) => {
         if(err) res.send(err)
         Object.assign(Applicant, req.body).save((err, applicant) => {
             if(err) res.send(err)
-            res.json({ message: 'Applicant updated!', Applicant })
+            res.json({ message: 'Applicant successfully updated!', Applicant })
         }) 
     })
 }
