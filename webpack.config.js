619e8494986daf215f40aea0ce12a145bfc5adf6
nsarafa <nicksarafa@gmail.com>
2017-03-29 17:16:28 -0400

    setup WebPack dev to proxy localhost:3000

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: true,
+    proxy: {'**': 'http://localhost:3000'}
   },
   performance: {
     hints: false
