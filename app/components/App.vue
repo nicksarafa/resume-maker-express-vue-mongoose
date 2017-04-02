1be12d75a325b3f919bed3dc55c80421065a8562
nsarafa <nicksarafa@gmail.com>
2017-04-02 04:40:50 -0400

    Experience init

--- app/components/App.vue
+++ app/components/App.vue
@@ -22,6 +22,47 @@
         >
       </form>
 
+      <!-- Experience <form /> -->
+      <!-- <form /> targets hiddenFrame to prevent uri redirects -->
+      <form action="/Experience" method="POST" target="hiddenFrame">
+        <input
+          name="organizationName"
+          type="text"
+          placeholder="Organization"
+          autofocus
+        >
+        <input
+          name="title"
+          type="text"
+          placeholder="Title"
+        >
+        <input
+          name="startMonth"
+          type="date"
+          placeholder="From Month"
+        >
+        <input
+          name="startYear"
+          type="date"
+          placeholder="From Year"
+        >
+        <input
+          name="endMonth"
+          type="date"
+          placeholder="To Month"
+        >
+        <input
+          name="endYear"
+          type="date"
+          placeholder="To Year"
+        >
+        <input
+          name="description"
+          type="text"
+          placeholder="Summary"
+        >
+      </form>
+
       <!-- Education <form /> -->
       <!-- <form /> targets hiddenFrame to prevent uri redirects -->
       <!-- @todo edit placeholders so we're not completley ripping off LinkedIn' -->
