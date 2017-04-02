9a747805d161ffe22c55323b7e8149583f22d3b9
nsarafa <nicksarafa@gmail.com>
2017-04-01 23:53:02 -0400

    remove skills module

--- app/vuex/store.js
+++ app/vuex/store.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import skills from './modules/skills'
 
 const debug = process.env.NODE_ENV !== 'production'
 
@@ -8,6 +7,5 @@ Vue.use(Vuex)
 Vue.config.debug = true
 
 export default new Vuex.Store({
-  modules: { skills },
   strict: debug,
 })
