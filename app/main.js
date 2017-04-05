6921b07f760613d42f6a74fc70cc9b89ef95dda5
nsarafa <nicksarafa@gmail.com>
2017-04-05 04:14:20 -0400

    kill vue-resource

--- app/main.js
+++ app/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import App from './components/App.vue'
-import VueResource from 'vue-resource'
 import store from './vuex'
-
-Vue.use(VueResource)
 
 new Vue({
   store,
-  http: { root: '/'},
+  http: { root: '/' },
   el: '#app',
   render: h => h(App)
 })
